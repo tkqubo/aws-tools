3567a23dc8523846e3d5dbbd2861c61a089714c2
j.kugiya <kugiya@oneteam.co.jp>
2016-12-13 12:27:24 +0900

    Adds codes for unreserved characters

--- aws-v4-signer-scala/src/main/scala/jkugiya/awstools/signer/v4/HttpRequest.scala
+++ aws-v4-signer-scala/src/main/scala/jkugiya/awstools/signer/v4/HttpRequest.scala
@@ -1,12 +1,16 @@
 package jkugiya.awstools.signer.v4
 
-import java.net.URI
+import java.net.{ URI, URLEncoder }
 
 case class HttpRequest(method: String, uri: URI) {
 
   val path =
     if (uri.getPath.isEmpty) "/"
-    else uri.getPath
+    else {
+      "/" +
+        URLEncoder.encode(uri.getPath.substring(1, uri.getPath.length), "UTF-8")
+        .replaceAll("\\*", "%2A")
+    }
 
   val query =
     Option(uri.getQuery).getOrElse("")
